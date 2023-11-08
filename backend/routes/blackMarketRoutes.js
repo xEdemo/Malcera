35e83d21deea14db343f2e777a4d34f3446799c6
xEdemo <136264312+xEdemo@users.noreply.github.com>
2023-11-07 22:22:42 -0500

    Update blackMarketRoutes.js

--- backend/routes/blackMarketRoutes.js
+++ backend/routes/blackMarketRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const blackMarketController = require('../controllers/blackMarketController');
+const blackMarketController = require('../controllers/blackMarketController.js');
 
 // Create a new Black Market
 router.post('/', blackMarketController.createBlackMarket);
