096b2fef84a4d584033b96ea48f858150718d0e6
xEdemo <136264312+xEdemo@users.noreply.github.com>
2023-11-07 22:28:37 -0500

    Update forbiddenlibraryRoutes.js

--- backend/routes/forbiddenlibraryRoutes.js
+++ backend/routes/forbiddenlibraryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const forbiddenLibraryController = require('../controllers/forbiddenlibraryController');
+const forbiddenLibraryController = require('../controllers/forbiddenLibraryController');
 
 // Create a new Forbidden Library
 router.post('/', forbiddenLibraryController.createForbiddenLibrary);
