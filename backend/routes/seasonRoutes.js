49724542c7cc2e17239c29a75d74c0371bec21b3
xEdemo <136264312+xEdemo@users.noreply.github.com>
2023-11-07 22:38:43 -0500

    Update seasonRoutes.js

--- backend/routes/seasonRoutes.js
+++ backend/routes/seasonRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const seasonController = require('../controllers/seasonController');
+const seasonController = require('../controllers/seasonController.js');
 
 // Get all seasons
 router.get('/', seasonController.getSeasons);
