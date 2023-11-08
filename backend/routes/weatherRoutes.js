85ba62a9ba16fae01f438f01cd7f8f15071c8ddb
xEdemo <136264312+xEdemo@users.noreply.github.com>
2023-11-07 22:42:31 -0500

    Update weatherRoutes.js

--- backend/routes/weatherRoutes.js
+++ backend/routes/weatherRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const weatherController = require('../controllers/weatherController');
+const weatherController = require('../controllers/weatherController.js');
 
 // Create a new weather condition
 router.post('/', weatherController.createWeather);
